ccc7109a1071da683af9d9bdb668cb95a9e0a9a8
Txr71 <lucasa7vs@gmail.com>
2026-02-05 08:19:34 -0500

    prodution

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,7 @@ module.exports = {
   apps: [
     {
       name: "botpro-bot",
-      script: "bot/index.js",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      env: {
-        NODE_ENV: "production"
-      }
-    },
-    {
-      name: "botpro-web",
-      script: "web/app.js",
+      script: "bot/index.js",  // caminho relativo
       instances: 1,
       autorestart: true,
       watch: false,
